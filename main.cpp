a0a0e00788f4b4dd7109364c710944fc75504b25
Patrick Clausen <201811369@post.au.dk>
2021-12-19 17:55:20 +0100

    Written socket functionality

--- main.cpp
+++ main.cpp
@@ -1,31 +1,12 @@
-#include <iostream>
 #include <boost/asio.hpp>
+#include "SocketManager.h"
 
-using boost::asio::ip::tcp;
-
-std::string read(tcp::socket& socket) {
-    char recv[1024];
-
-    socket.receive(boost::asio::buffer(recv));
-    std::string data = recv;
-    return data;
-}
+using namespace boost::asio;
 
 int main() {
-    boost::asio::io_context io_context;
-    tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 80));
-    for (;;) {
-        tcp::socket socket(io_context);
-        acceptor.accept(socket);
-
-        std::string message = read(socket);
-
-        std::cout << message << std::endl;
-
+    SocketManager manager(ip::tcp::endpoint(ip::tcp::v4(), 80));
+    for(;;) {
 
     }
-
-
-    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
