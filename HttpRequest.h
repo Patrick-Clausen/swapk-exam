1d0978687d0bcc7fe19d1d6b7dc9f63c519bd9a4
Tueaj <47721101+Tueaj@users.noreply.github.com>
2021-12-19 17:45:06 +0100

    Add cout to request

--- HttpRequest.h
+++ HttpRequest.h
@@ -12,6 +12,19 @@ struct HttpRequest : public HttpCall {
     std::string path;
     std::string method;
     std::string protocolVersion;
+
+    friend std::ostream &operator<<( std::ostream &output, const HttpRequest &httpRequest ) {
+        output << httpRequest.method << " " << httpRequest.path << " " << httpRequest.protocolVersion << std::endl;
+
+        for (auto it = httpRequest.headers.begin(); it != httpRequest.headers.end(); it++)
+        {
+            output << it->first << ": " << it->second << std::endl;
+        }
+
+        output << httpRequest.body;
+
+        return output;
+    }
 };
 
 #endif //SWAPK_EXAM_HTTPREQUEST_H
