5c7a12038eb8aba23a9ccdfd8a67885f9c29e935
Tueaj <47721101+Tueaj@users.noreply.github.com>
2021-12-19 17:38:00 +0100

    Parser and stuff made

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
+#include "HttpCallParser.h"
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::string input =
+            "POST / HTTP/1.1\nHost: localhost\nUser-Agent: curl/7.55.1\nAccept: /\nContent-Length: 5\nContent-Type: application/x-www-form-urlencoded\n\n{\nTemperature: 5\nCountry: Denmark\n}";
+    auto parser = HttpCallParser();
+    auto request = parser.ParseCallToResponse(input);
+
+    std::cout << request << std::endl;
     return 0;
 }
