fa96f3e308c0ee71883eb8e527a5e0bb64e82aa7
Tueaj <47721101+Tueaj@users.noreply.github.com>
2021-12-19 17:45:06 +0100

    Add cout to request

--- main.cpp
+++ main.cpp
@@ -5,8 +5,10 @@ int main() {
     std::string input =
             "POST / HTTP/1.1\nHost: localhost\nUser-Agent: curl/7.55.1\nAccept: /\nContent-Length: 5\nContent-Type: application/x-www-form-urlencoded\n\n{\nTemperature: 5\nCountry: Denmark\n}";
     auto parser = HttpCallParser();
+    auto response = parser.ParseCallToResponse(input);
     auto request = parser.ParseCallToResponse(input);
 
+    std::cout << response << std::endl;
     std::cout << request << std::endl;
     return 0;
 }
