8c3c12c82c183302b9b7bcd8a92f0514e8906fa7
Patrick Clausen <201811369@post.au.dk>
2021-12-20 16:56:51 +0100

    Enum-ified parsing code

---
+++ http/HTTPEnums.h
@@ -0,0 +1,35 @@
+//
+// Created by Patrick Clausen on 20/12/2021.
+//
+
+#ifndef SWAPK_EXAM_HTTPENUMS_H
+#define SWAPK_EXAM_HTTPENUMS_H
+
+#include <map>
+#include <string>
+
+enum ProtocolVersion {
+    HTTP_1_0,
+    HTTP_1_1,
+    HTTP_2
+};
+
+enum RequestMethod {
+    GET,
+    HEAD,
+    POST,
+    PUT,
+    DELETE,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    PATCH
+};
+
+template<typename T>
+extern std::string getStringFromEnumValue(T enumValue);
+
+template<typename T>
+extern T getEnumValueFromString(std::string& str);
+
+#endif //SWAPK_EXAM_HTTPENUMS_H
--- http/HTTPRequest.h
+++ http/HTTPRequest.h
@@ -4,13 +4,14 @@
 
 #ifndef SWAPK_EXAM_HTTPREQUEST_H
 #define SWAPK_EXAM_HTTPREQUEST_H
+
 #include <string>
 #include "HTTPCall.h"
+#include "HTTPEnums.h"
 
 struct HTTPRequest : public HTTPCall {
     std::string path;
-    std::string method;
-    std::string protocolVersion;
+    RequestMethod method;
 };
 
 #endif //SWAPK_EXAM_HTTPREQUEST_H
