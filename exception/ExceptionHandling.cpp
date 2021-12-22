9559ac90c0ba3d2ca17eb47ca99d70d2a37cffe6
Patrick Clausen <201811369@post.au.dk>
2021-12-22 16:12:20 +0100

    Cleanup after merging main into exception handling

--- exception/ExceptionHandling.cpp
+++ exception/ExceptionHandling.cpp
@@ -2,18 +2,18 @@
 // Created by Tue on 20/12/2021.
 //
 
-#include "ExeptionHandling.h"
+#include "ExceptionHandling.h"
 
 void ExceptionHandling::AddException(const std::exception& exception, std::string_view statusCode, std::string_view statusMessage) {
-    HttpResponse response;
+    HTTPResponse response;
     response.statusCode = statusCode;
-    response.protocolVersion = "Http/1.1";
+    response.protocolVersion = ProtocolVersion::HTTP_1_1;
     response.statusMessage = statusMessage;
     exceptionMapping.insert({typeid(exception).name(), response});
 }
 
-HttpResponse ExceptionHandling::HandleException(const std::exception& exception) {
-    HttpResponse response = exceptionMapping.find(typeid(exception).name())->second;
+HTTPResponse ExceptionHandling::HandleException(const std::exception& exception) {
+    HTTPResponse response = exceptionMapping.find(typeid(exception).name())->second;
     response.body = exception.what();
     return response;
 }
