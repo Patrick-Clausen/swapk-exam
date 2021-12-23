3e8173b4673a43d0cd6ca3887a7d2954cad6b83f
Einar Hansson <einarjorgen.hansson@ejh.dk>
2021-12-23 10:34:01 +0100

    Added default constructor to HTTPResponse.h and used it in Dispatcher.cpp

--- http/HTTPResponse.h
+++ http/HTTPResponse.h
@@ -5,9 +5,17 @@
 #ifndef SWAPK_EXAM_HTTPRESPONSE_H
 #define SWAPK_EXAM_HTTPRESPONSE_H
 #include <string>
+#include <utility>
 #include "HTTPCall.h"
 
 struct HTTPResponse : public HTTPCall {
+    explicit HTTPResponse(std::string responseBody) {
+        statusCode = "200";
+        statusMessage = "OK";
+        protocolVersion = ProtocolVersion::HTTP_1_1;
+        body = std::move(responseBody);
+    }
+
     std::string statusMessage;
     std::string statusCode;
 };
