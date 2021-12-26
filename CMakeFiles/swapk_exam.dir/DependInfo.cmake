3d302d32c9c4b4c349c69fa695817e5fec545e1a
Einar Hansson <einarjorgen.hansson@ejh.dk>
2021-12-26 16:46:16 +0100

    Added cmake files

---
+++ CMakeFiles/swapk_exam.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/einar/skole/swapk-exam/RESTBuilder.cpp" "/home/einar/skole/swapk-exam/CMakeFiles/swapk_exam.dir/RESTBuilder.cpp.o"
+  "/home/einar/skole/swapk-exam/controller-examples/WeatherController.cpp" "/home/einar/skole/swapk-exam/CMakeFiles/swapk_exam.dir/controller-examples/WeatherController.cpp.o"
+  "/home/einar/skole/swapk-exam/dispatch/Dispatcher.cpp" "/home/einar/skole/swapk-exam/CMakeFiles/swapk_exam.dir/dispatch/Dispatcher.cpp.o"
+  "/home/einar/skole/swapk-exam/exception/ExceptionHandler.cpp" "/home/einar/skole/swapk-exam/CMakeFiles/swapk_exam.dir/exception/ExceptionHandler.cpp.o"
+  "/home/einar/skole/swapk-exam/exception/defaults/endpoint_not_found_exception.cpp" "/home/einar/skole/swapk-exam/CMakeFiles/swapk_exam.dir/exception/defaults/endpoint_not_found_exception.cpp.o"
+  "/home/einar/skole/swapk-exam/exception/defaults/method_not_allowed_exception.cpp" "/home/einar/skole/swapk-exam/CMakeFiles/swapk_exam.dir/exception/defaults/method_not_allowed_exception.cpp.o"
+  "/home/einar/skole/swapk-exam/exception/defaults/parse_failure_exception.cpp" "/home/einar/skole/swapk-exam/CMakeFiles/swapk_exam.dir/exception/defaults/parse_failure_exception.cpp.o"
+  "/home/einar/skole/swapk-exam/http/HTTPCallParser.cpp" "/home/einar/skole/swapk-exam/CMakeFiles/swapk_exam.dir/http/HTTPCallParser.cpp.o"
+  "/home/einar/skole/swapk-exam/http/HTTPEnums.cpp" "/home/einar/skole/swapk-exam/CMakeFiles/swapk_exam.dir/http/HTTPEnums.cpp.o"
+  "/home/einar/skole/swapk-exam/http/details/HTTPCallHandler.cpp" "/home/einar/skole/swapk-exam/CMakeFiles/swapk_exam.dir/http/details/HTTPCallHandler.cpp.o"
+  "/home/einar/skole/swapk-exam/main.cpp" "/home/einar/skole/swapk-exam/CMakeFiles/swapk_exam.dir/main.cpp.o"
+  "/home/einar/skole/swapk-exam/socket/SocketConnection.cpp" "/home/einar/skole/swapk-exam/CMakeFiles/swapk_exam.dir/socket/SocketConnection.cpp.o"
+  "/home/einar/skole/swapk-exam/socket/SocketListener.cpp" "/home/einar/skole/swapk-exam/CMakeFiles/swapk_exam.dir/socket/SocketListener.cpp.o"
+  "/home/einar/skole/swapk-exam/socket/SocketManager.cpp" "/home/einar/skole/swapk-exam/CMakeFiles/swapk_exam.dir/socket/SocketManager.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/swapk_exam.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/swapk_exam.dir/RESTBuilder.cpp.o"
+  "CMakeFiles/swapk_exam.dir/controller-examples/WeatherController.cpp.o"
+  "CMakeFiles/swapk_exam.dir/dispatch/Dispatcher.cpp.o"
+  "CMakeFiles/swapk_exam.dir/exception/ExceptionHandler.cpp.o"
+  "CMakeFiles/swapk_exam.dir/exception/defaults/endpoint_not_found_exception.cpp.o"
+  "CMakeFiles/swapk_exam.dir/exception/defaults/method_not_allowed_exception.cpp.o"
+  "CMakeFiles/swapk_exam.dir/exception/defaults/parse_failure_exception.cpp.o"
+  "CMakeFiles/swapk_exam.dir/http/HTTPCallParser.cpp.o"
+  "CMakeFiles/swapk_exam.dir/http/HTTPEnums.cpp.o"
+  "CMakeFiles/swapk_exam.dir/http/details/HTTPCallHandler.cpp.o"
+  "CMakeFiles/swapk_exam.dir/main.cpp.o"
+  "CMakeFiles/swapk_exam.dir/socket/SocketConnection.cpp.o"
+  "CMakeFiles/swapk_exam.dir/socket/SocketListener.cpp.o"
+  "CMakeFiles/swapk_exam.dir/socket/SocketManager.cpp.o"
+  "swapk_exam"
+  "swapk_exam.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/swapk_exam.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
