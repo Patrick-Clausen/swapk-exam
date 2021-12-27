5af80d0711b469ed12d507be10eb8d3fe9f5bda6
Einar Hansson <einarjorgen.hansson@ejh.dk>
2021-12-27 12:27:43 +0100

    Update shell script

--- test.sh
+++ test.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 LG='\e[1;32m'
 Y='\e[1;33m'
+REQUIRED_PKG=("cmake", "gnome-terminal")
+
 
 if [[ $EUID -ne 0 ]]; then
    echo -e "${LG}This script must be run as root${Y}" 
    exec sudo -- "$0" "$@"
 fi
 
+
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+echo Checking for $REQUIRED_PKG: $PKG_OK
+if [ "" = "$PKG_OK" ]; then
+  echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+  sudo apt-get --yes install $REQUIRED_PKG
+fi
 
 echo -e "${LG}Running Make${Y}"
 make
